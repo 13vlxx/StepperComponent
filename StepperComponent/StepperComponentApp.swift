bf0820f9e52206e648ce2de820b9f1c9d4fab150
ALX <alexmonac13@gmail.com>
2023-07-18 18:16:21 +0200

    Initial Commit

---
+++ StepperComponent/StepperComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperComponentApp.swift
+//  StepperComponent
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepperComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
