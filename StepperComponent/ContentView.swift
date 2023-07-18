595790a38588043aebc07c96afc9cd1d66f583b7
ALX <alexmonac13@gmail.com>
2023-07-18 18:33:37 +0200

    Label stepper component

--- StepperComponent/ContentView.swift
+++ StepperComponent/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var value: Double = 0
+    @State private var value2: Double = 0
+    @State private var value3: Double = 0
+    @State private var value4: Double = 0
+    @State private var value5: Double = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            // Step normal
+            Section {
+                VStack {
+                    Text("Stepper Simple")
+                    Stepper("Quantité : \(Int(value))", value: $value)
+                }
+            }
+            
+            // Step avec plage
+            Section {
+                VStack {
+                    Text("Stepper avec plage de 0 à 100")
+                    Stepper("Quantité : \(Int(value2))", value: $value2, in: 0...100)
+                }
+            }
+            
+            // Step avec pas de 10
+            Section {
+                VStack {
+                    Text("Stepper avec plage de 0 à 100")
+                    Stepper("Quantité : \(Int(value3))", value: $value3, in: 0...100, step: 10)
+                }
+            }
+            
+            // Step avec option increment/décrément
+            Section {
+                VStack {
+                    Text("Stepper avec plage de 0 à 100")
+                    Stepper("Quantité : \(Int(value4))") {
+                        self.value4 += 1
+                    } onDecrement: {
+                        self.value4 -= 1
+                    }
+
+                }
+            }
+            
+            // Step avec label
+            Section {
+                VStack {
+                    Text("Stepper avec label")
+                    Stepper {
+                        self.value5 += 1
+                    } onDecrement: {
+                        self.value5 -= 1
+                    } label: {
+                        HStack {
+                            Image(systemName: "pencil.circle.fill").font(.largeTitle)
+                            Text("Quantité : \(Int(value5))")
+                        }
+                    }
+
+                }
+            }
         }
-        .padding()
     }
 }
 
